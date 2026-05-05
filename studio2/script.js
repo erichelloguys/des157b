cc5d7a59301ce7806e10b4e9a5fbae90f3f3ae00
Hanjie Chen <hjcchen@ucdavis.edu>
2026-05-04 18:03:28 -0700

    made final updates to the car movement setting a fixed vh vw maxWidth and changing it using js properties according to json dataPoints

--- studio2/script.js
+++ studio2/script.js
@@ -1,58 +1,108 @@
-(function(){
+(function () {
     'use strict';
 
     let globalData;
 
-    async function getData(){
+    async function getData() {
         const travelLog = await fetch('bayAreaTravel.json')
         const data = await travelLog.json();
 
         globalData = data;
 
         document.querySelector('#picker').innerHTML = createSelectList(data);
-
-        document.querySelector('#purpose').innerHTML = outputHTML(data);
     }
 
-    function createSelectList(data){
-        let html = '<option value="">--Select a month--</option>';
-
+    function createSelectList(data) {
+        let selectList = '<option value="">--Select a month--</option>';
         const dataPoints = Object.keys(data);
-        
-        dataPoints.forEach(function(eachPoint){
-            html += `<option value="${eachPoint}">${data[eachPoint].months}</option>`;
+
+        dataPoints.forEach(function (eachPoint) {
+            selectList += `<option value="${eachPoint}">${data[eachPoint].months}</option>`;
         })
 
-        return html;
+        return selectList;
     }
 
-    document.querySelector('#picker').addEventListener('change', function(){
+    // const car = document.querySelector('#car');
+    // const carSize = car.getBoundingClientRect();
+
+    // let carX = carSize.left + window.scrollX;
+    // let carY = carSize.top + window.scrollY;
+
+    // console.log(carSize);
+    // console.log(carX, carY);
+
+    // function moveCar(data){
+    //     const dataPoints = Object.keys(data);
+    //     console.log(dataPoints);
+
+    //     dataPoints.forEach(function(eachPoint){
+    //         if (dataPoint < 1){
+    //             carX.style.left += 10;
+    //             carY.style.top += 10;
+    //         }
+    //     })
+    // }
+
+    function getIndex(value, data) {
+        const dataPoints = Object.keys(data);
+        return dataPoints.indexOf(value);
+    }
+
+    const oldX = -65;
+    const oldY = 5;
+    const oldWidth = 30;
+
+    function resetCarValue() {
+        car.style.left = `${oldX}vw`;
+        car.style.bottom = `${oldY}vh`;
+        car.style.maxWidth = `${oldWidth}vw`
+    }
+
+    function moveCar(value, data) {
+        const index = getIndex(value, data);
+
+        const moveX = 2;
+        const moveY = -0.75;
+
+        const newX = oldX + (moveX * index);
+        const newY = oldY + (moveY * index);
+        let newWidth = oldWidth + (oldWidth * index / 7);
+
+        car.style.left = `${newX}vw`;
+        car.style.bottom = `${newY}vh`;
+        car.style.maxWidth = `${newWidth}vw`;
+    }
+
+
+    let html = document.querySelector('#purpose');
+    let emptyArea = document.querySelector('#emptyArea');
+
+    document.querySelector('#picker').addEventListener('change', function () {
+        // event.preventDefault();
         const newValue = this.value;
 
-        if (newValue){
+        if (newValue) {
             updateInterface(newValue, globalData);
             updateEmptyArea(newValue, globalData);
+            moveCar(newValue, globalData);
+            // carX += ("10vw");
         } else {
-            document.querySelector('#purpose').innerHTML = "";
+            html.innerHTML = "";
 
             emptyArea.innerHTML = "<h2 id='billboardText'>Initializing...<h2>";
             emptyArea.style.backgroundImage = "none";
+
+            resetCarValue();
         }
     })
 
-    function updateInterface(value, jsonData){
+    function updateInterface(value, jsonData) {
 
-        let html = '<p>';
-
-        html += `In ${jsonData[value].months} I made <a id="numberOfTrip">${jsonData[value].trips}</a> trips back to the Bay area because: ${jsonData[value].purpose}`;
-
-        html += '</p>';
-        document.querySelector('#purpose').innerHTML = html;
+        html.innerHTML = `<p>In ${jsonData[value].months} I made <a id="numberOfTrip">${jsonData[value].trips}</a> trips back to the Bay area because: ${jsonData[value].purpose}</p>`;
     }
 
-    let emptyArea = document.querySelector('#emptyArea');
-
-    function updateEmptyArea(value, jsonData){
+    function updateEmptyArea(value, jsonData) {
         emptyArea.style.backgroundImage = `url(${jsonData[value].image})`;
         emptyArea.innerHTML = "";
     }
